9f5fbf5260c2b6cbc79aa7dce684d4131b97e0d1
Nata Braun <pushistik2030@gmail.com>
2023-02-02 00:58:12 +0300

    Task is solved

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 'c'; char charH = 'h'; char charA = 'a'; char charR = 'r';
+        int c1 = charC;
+        int h1 = charH;
+        int a1 = charA;
+        int r1 = charR;
+        System.out.println(99);
+        System.out.println(104);
+        System.out.println(97);
+        System.out.println(114);
+
+
     }
 }
